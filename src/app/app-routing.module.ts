a7ecdcaa7c047e913e335824f2582accc419fd6b
Trung Phạm <TrungPham@users.noreply.github.com>
2021-09-12 19:15:37 +0700

    SignIn page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'signin',
+    loadChildren: () => import('./pages/signin/signin.module').then( m => m.SigninPageModule)
   },
+
 
 ];
 
