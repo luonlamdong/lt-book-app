a52f94a53f28931da9a4c26b138aaa5f6da6fe72
Thái Duy Linh <linhmk2905#gmail.com>
2021-09-13 09:30:07 +0700

    update code home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  option = {
+    loop: true,
+    direction: 'vertical'
+  };
   constructor() {}
 
 }
