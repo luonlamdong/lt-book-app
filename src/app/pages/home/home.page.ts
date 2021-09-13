aa969a13f1c68dde19e612646e716f84bc626dff
Thái Duy Linh <linhmk2905#gmail.com>
2021-09-13 09:55:21 +0700

    update play music

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   option = {
     loop: true,
     direction: 'vertical'
   };
+  rotate360: any = false;
   constructor() {}
+
+  ngOnInit(){
+
+  }
+
+  rotateActive(){
+    if(this.rotate360 == false){
+      this.rotate360 = true;
+    }
+    else{
+      this.rotate360 = false;
+    }
+  }
 
 }
